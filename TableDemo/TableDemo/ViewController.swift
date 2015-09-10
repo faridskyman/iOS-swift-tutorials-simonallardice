e30be4c2fd9856e1623325274bc04b81a10f9048
farid <farid@eits-MacBook-Air.local>
2015-09-10 16:32:09 +0800

    completed table view with 2 sections

--- TableDemo/TableDemo/ViewController.swift
+++ TableDemo/TableDemo/ViewController.swift
@@ -16,7 +16,20 @@ class ViewController: UIViewController, UITableViewDataSource {
         ("Mobile project", "Task and Preview"),
         ("Go to LMS", "SSO to the LMS")
     ]
+    
+    let LMSToolsMenu = [
+        ("Attendance", "Manage student attendances"),
+        ("Enrollment", "Manage course enrollments"),
+        ("Tutor Admin", "manage student accounts"),
+        ("NAPFA", "Fitness Tracking Applic"),
+        ("RMS", "Resource Booking system"),
+        ("eDiscipline", "Manage student conducts")
+    ]
 
+
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -31,28 +44,38 @@ class ViewController: UIViewController, UITableViewDataSource {
     // table view deligate that needs to be implemented
     // creates a table with one section, 5 rows and each shows "hello"
 
-
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1;
+        return 2;
     }
     
+    
+    // for table view to know count based on section selected.
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         //
-        return LMSAppMenu.count
+        if section == 0 {
+            return LMSAppMenu.count
+        }
+        else {
+            return LMSToolsMenu.count
+        }
     }
     
     
     /// based on 'numberof rows in section' as its return 5, this is called 5 times.
-    // below is updated to 
+    // below is updated to have better memory managememt. 'reusing cell' dequeue.
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        //
-        let (LMSModule, Descriptin) = LMSAppMenu[indexPath.row]
+        //var cell = UITableViewCell()
         
-        var cell = UITableViewCell()
-        cell.textLabel?.text = LMSModule
+        var cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as UITableViewCell
         
+        if indexPath.section == 0 {
+            let (LMSModule, Descriptin) = LMSAppMenu[indexPath.row]
+            cell.textLabel?.text = LMSModule
+        } else {
+            let (LMSModule, Descriptin) = LMSToolsMenu[indexPath.row]
+            cell.textLabel?.text = LMSModule
+        }
         return cell
-        
     }
     
     
